937bcbc44dc014c056c618d1f3bbe0e81d48d50b
Harshvardhan Sisodiya <297878778+harshvardhansisodiya19-pixel@users.noreply.github.com>
2026-07-03 08:15:49 +0530

    Add files via upload

---
+++ CH2 practice.py
@@ -0,0 +1,24 @@
+a=1
+b=5
+print(a+b)
+
+#problem 2
+a=34
+b-12
+print("Remainder when a is divided by b is: ", a%b)
+
+#problem 3
+a=input("Enter a number 1: ")
+print(type(a))
+#Problem 4
+a=input("Enter a number 1: ")
+b=input("Enter a number 2: ")
+a=int(a)
+b=int(b)
+print(a>b)#Shows false or true based on value like a=34 and b =67=>false
+#Problem 5 to find average of two numbers
+print("The average of these two numbers is: ", (a+b)/2)
+
+#Problem 6
+print("The square of a is: ", a**2)
+#a^2 not valid you can use **
