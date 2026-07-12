0f74b8ffa00e4c1b5c550f566d3a0513cb8402a7
Harshvardhan Sisodiya <297878778+harshvardhansisodiya19-pixel@users.noreply.github.com>
2026-07-12 09:32:00 +0530

    Add files via upload

---
+++ Chapter6.py
@@ -0,0 +1,24 @@
+#CONDITIONALS
+a=int(input("Enter your age:"))
+#If elif else ladder
+#If statement one
+if(a%2==0):
+    print("a is even")# both if independent
+#If statement 2
+if(a>18):
+    print("You are above the age of consent")
+    print("Good for you")
+elif(a<0):
+    print("Age cannot be negative")
+
+elif(0<a<18):
+    print("You are below the age of consent")
+
+
+print("End of program")
+#Else is used only if satetment if fails
+#Elif when multiple condition
+#else KE SAATH NO BRACKTES OF CONDITION 
+ # Reln operators == >= <=
+ # and or not = Logical Operators
+ # if can be single but else and elif cannot
